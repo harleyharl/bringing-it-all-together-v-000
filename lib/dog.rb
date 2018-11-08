526a465acb7d5fb81384b1f4068142fb9e3a68eb
Harley Rayner <harleymteden@gmail.com>
2018-11-08 19:40:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,7 +58,7 @@ def self.find_or_create_by(name:,breed:)
   end
 end
 
-def self.new_from_db
+def self.new_from_db(db_row)
   # DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
 end
 
